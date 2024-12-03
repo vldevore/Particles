b39f650b49a50d481ade5ebd72237043b8403be0
vldevore <w2078153@apps.losrios.edu>
2024-12-02 20:07:01 -0800

    Update Engine.cpp

--- code/Engine.cpp
+++ code/Engine.cpp
@@ -1,1 +1,28 @@
+#include "Engine.h"
+using namespace sf;
+using namespace std;
 
+void Engine::input()
+{
+
+}
+
+void Engine::update(float dtAsSeconds)
+{
+
+}
+
+void Engine::draw()
+{
+
+}
+
+Engine::Engine()
+{
+
+}
+
+void Engine::run()
+{
+
+}
