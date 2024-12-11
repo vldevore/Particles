de7e6bdc0984a82ac9f1c1913e670663325f7238
vldevore <w2078153@apps.losrios.edu>
2024-12-11 10:53:28 -0800

    Update Particle.cpp

--- code/Particle.cpp
+++ code/Particle.cpp
@@ -50,12 +50,12 @@ Particle::Particle(RenderTarget &target, int numPoints, Vector2i mouseClickPosit
 void Particle::draw(RenderTarget &target, RenderStates states) const
 {
     sf::VertexArray lines(TriangleFan, m_numPoints + 1);
-    Vector2f center = target.mapCoordsToPixel(m_centerCoordinate, m_cartesianPlane);
+    Vector2f center = Vector2f(target.mapCoordsToPixel(m_centerCoordinate, m_cartesianPlane));
     lines[0].position = center;
     lines[0].color = m_color1;
     for (int j = 1; j < (m_numPoints + 1); j++)
     {
-        lines[j].position = Vector2f(target.mapCoordsToPixel(m_A( 0, j - 1), m_cartesianPlane));
+        lines[j].position = Vector2f(target.mapCoordsToPixel(m_A(0, j - 1), m_cartesianPlane));
         lines[j].color = m_color2;
     }
     target.draw(lines);
